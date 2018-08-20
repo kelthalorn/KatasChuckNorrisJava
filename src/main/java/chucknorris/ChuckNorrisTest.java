8262473a0c788052e3726a4d90ea49fb560671c9
kelthalorn <tibo.colin@gmail.com>
2018-08-20 11:38:52 +0200

    ADD: first tu

--- src/main/java/chucknorris/ChuckNorrisTest.java
+++ src/main/java/chucknorris/ChuckNorrisTest.java
@@ -1,5 +1,18 @@
 package chucknorris;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ChuckNorrisTest {
 
+    @Test
+    public void chuckNorrisShouldReturn0_0_00_0000_0_00IfReceiveC() {
+        //Given
+        ChuckNorris chuckNorris = new ChuckNorris("C");
+        String expected = "0 0 00 0000 0 00";
+        //When
+        chuckNorris.convertBinaryStringToUnaryString();
+        //Then
+        Assert.assertEquals(expected, chuckNorris.getOutput());
+    }
 }
